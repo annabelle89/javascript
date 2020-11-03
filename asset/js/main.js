0325aee3cf6a679554a899a191bfc5db5516fcd2
Annabelle hoarau <annabellehoarau1989+lamanu@gmail.com>
2020-11-03 14:57:02 +0100

    exercice5

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,6 +1,15 @@
-function sendData() {
+function divideThem() {
     var firstnumber = document.getElementById('firstnumber').value;
     var secondnumber = document.getElementById('secondnumber').value;
+    var result = (firstnumber % secondnumber);
+    alert(résultat);
+}
 
-    alert('premier résultat = ' + firstnumber % secondnumber);
+function displayAlert() {
+    var myResult = divideThem();
+    alert(myResult);
+}
+/*---ou---*/
+function displayAlert() {
+    alert(divideThem());
 }
