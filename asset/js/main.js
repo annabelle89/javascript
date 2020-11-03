6519b0106f47dba674b72722bafbb3f8b6d92890
Annabelle hoarau <annabellehoarau1989+lamanu@gmail.com>
2020-11-03 11:24:25 +0100

    exercice4etcie

---
+++ asset/js/main.js
@@ -0,0 +1,12 @@
+let x = 4;
+let y = 0;
+
+if (x > 1) {
+    alert('x contient une valeur strictement supérieure à 1');
+}
+if (x == y) {
+    alert('x et y contiennent la même valeur');
+}
+if (y) {
+    alert('la valeur de y est évaluée à true');
+}
