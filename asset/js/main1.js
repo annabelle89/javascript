8299c801410f0ea0ab8a1abc6f70d138badf916d
Annabelle hoarau <annabellehoarau1989+lamanu@gmail.com>
2020-11-03 17:12:25 +0100

    exercicesuivant

--- asset/js/main1.js
+++ asset/js/main1.js
@@ -1,6 +1,6 @@
-function divideThem() {
+function calculThem() {
     var shoeSize = document.getElementById('shoeSize').value;
     var birth = document.getElementById('birth').value;
-    var result = (shoeSize * 2) + 5) * 50) - birth) + 1769);
-return (result);
+    var result = (((((shoeSize * 2) + 5) * 50) - birth) + 1769);
+    alert('le résultat est: ' + result);
 }
