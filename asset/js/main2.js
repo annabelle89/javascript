103a620313dd5bb564d4d9722e62330bd672293e
Annabelle hoarau <annabellehoarau1989+lamanu@gmail.com>
2020-11-04 10:16:41 +0100

    exercice7

---
+++ asset/js/main2.js
@@ -0,0 +1,8 @@
+function condition() {
+    var age = document.getElementById('age').value;
+    if (age > 18) {
+        alert('tu es majeur')
+    } else {
+        alert('tu es mineur');
+    }
+}
